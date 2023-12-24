acbd724d3f0c99be282e7722e13ddc9724c4a453
Victor Ferraz <victor@MacBook-Pro-de-Victor.local>
2023-12-24 16:12:53 -0300

    gitsigns

--- lua/shalashaska/packer.lua
+++ lua/shalashaska/packer.lua
@@ -4,38 +4,38 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	} 
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	use { "rose-pine/neovim" }
-	use ("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"} ) 
-	use { "nvim-treesitter/playground" }
-	use { "theprimeagen/harpoon" }
-	use { "mbbill/undotree" }
-	use { "tpope/vim-fugitive" }
+    use { "rose-pine/neovim" }
+    use ("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"} )
+    use { "nvim-treesitter/playground" }
+    use { "theprimeagen/harpoon" }
+    use { "mbbill/undotree" }
+    use { "tpope/vim-fugitive" }
     use { "andweeb/presence.nvim" }
+    use { "lewis6991/gitsigns.nvim" }
 
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 end)
